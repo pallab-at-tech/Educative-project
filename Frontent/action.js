fd08efc8b49720789168387c4a834bd6a9dec44e
pallab-at-tech <pallab861774@gmail.com>
2025-01-13 21:02:34 +0530

    create intial body section and style them, also work on javascript for scroll up to disappear header and scroll down to appear.

--- Frontent/action.js
+++ Frontent/action.js
@@ -9,3 +9,24 @@ btn.onclick = function () {
 searchbtn.onclick = function () {
     sideber.classList.toggle("active");
 }
+
+
+// Hide Header on Scroll Down Show on Scroll Up.
+
+const nav = document.querySelector('.home-content');
+let lastScrollTop = 36;
+
+
+window.addEventListener('scroll', () => {
+
+    let scrollTop = document.documentElement.scrollTop;
+    console.log(scrollTop)
+
+    if (lastScrollTop < scrollTop && lastScrollTop>36) {
+        nav.classList.add('nav-hide');
+    }
+    else {
+        nav.classList.remove('nav-hide');
+    }
+        lastScrollTop = scrollTop;
+})
